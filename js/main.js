042e98df5288a8b481defd1a2a02ec93fee8fca6
Osmar Neto <osmarneto750@gmail.com>
2020-10-09 17:04:27 -0300

    Criando cobrinha

--- js/main.js
+++ js/main.js
@@ -1,12 +1,30 @@
 /* Feito por Osmar Januario de souza Neto */
 
-let canvas = document.getElementById('snake');
+// Variáveis
+let canvas = document.getElementById('cobrinha');
 let context = canvas.getContext("2d"); // Reindenização do canvas
 let box = 32;
+let Cobrinha = [];
 
-function CriarBG() { // Função que cria o background
-    context.fillStyle = "#33cc33";
+Cobrinha[0] = {
+    x: 10 * box,
+    y: 8 * box
+}
+
+
+// Função que cria o background
+function CriarBG() {
+    context.fillStyle = "#45a836";
     context.fillRect(0, 0, 26 * box, 16 * box);
 }
 
+// Função que cria cobrinha
+function CriarCobrinha() {
+    for(I=0; I < Cobrinha.length; I++){
+        context.fillStyle = "#111";
+        context.fillRect(Cobrinha[I].x, Cobrinha[I].y, box, box);
+    }
+}
+
 CriarBG();
+CriarCobrinha();
