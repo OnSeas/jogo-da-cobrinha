1d1b937e8892bcf643194cfcd4adb72e1baf352e
Osmar Neto <osmarneto750@gmail.com>
2020-10-09 16:43:18 -0300

    Criando background

--- js/main.js
+++ js/main.js
@@ -1,2 +1,12 @@
 /* Feito por Osmar Januario de souza Neto */
 
+let canvas = document.getElementById('snake');
+let context = canvas.getContext("2d"); // Reindenização do canvas
+let box = 32;
+
+function CriarBG() { // Função que cria o background
+    context.fillStyle = "#33cc33";
+    context.fillRect(0, 0, 26 * box, 16 * box);
+}
+
+CriarBG();
