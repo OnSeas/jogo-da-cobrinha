f559f343e95037f0441739fb458620574f6c739e
Osmar Neto <osmarneto750@gmail.com>
2020-10-10 09:58:14 -0300

    Fazendo cobrinha se movimentar

--- js/main.js
+++ js/main.js
@@ -1,30 +1,52 @@
 /* Feito por Osmar Januario de souza Neto */
 
 // Variáveis
-let canvas = document.getElementById('cobrinha');
-let context = canvas.getContext("2d"); // Reindenização do canvas
+let Canvas = document.getElementById('cobrinha');
+let Context = Canvas.getContext("2d"); // Reindenização do canvas
 let box = 32;
 let Cobrinha = [];
 
 Cobrinha[0] = {
-    x: 10 * box,
+    x: 12 * box,
     y: 8 * box
 }
 
+let Direction = "right";
 
 // Função que cria o background
 function CriarBG() {
-    context.fillStyle = "#45a836";
-    context.fillRect(0, 0, 26 * box, 16 * box);
+    Context.fillStyle = "#45a836";
+    Context.fillRect(0, 0, 26 * box, 16 * box);
 }
 
 // Função que cria cobrinha
 function CriarCobrinha() {
     for(I=0; I < Cobrinha.length; I++){
-        context.fillStyle = "#111";
-        context.fillRect(Cobrinha[I].x, Cobrinha[I].y, box, box);
+        Context.fillStyle = "#111";
+        Context.fillRect(Cobrinha[I].x, Cobrinha[I].y, box, box);
     }
 }
 
-CriarBG();
-CriarCobrinha();
+function IniciarJogo() {
+    CriarBG();
+    CriarCobrinha();
+
+    let CobrinhaX = Cobrinha[0].x;
+    let CobrinhaY = Cobrinha[0].y;
+
+    if(Direction == "right") CobrinhaX += box;
+    if(Direction == "left") CobrinhaX -= box;
+    if(Direction == "up") CobrinhaY -= box;
+    if(Direction == "Down") CobrinhaY += box;
+
+    Cobrinha.pop();
+
+    let NovaCabeca = {
+        x: CobrinhaX,
+        y: CobrinhaY
+    }
+
+    Cobrinha.unshift(NovaCabeca);
+}
+
+let Jogo = setInterval(IniciarJogo, 100);
